8896257bde025417adcf0d154b7bec7a3b932e7f
ThorXdshow <86394838+ThorXdshow@users.noreply.github.com>
2021-11-29 21:42:40 +0530

    Initial commit

---
+++ WebStreamer/__init__.py
@@ -0,0 +1,5 @@
+
+# Maintained By : ThorXdshow [ @ThorXdshow ] [ Telegram ]
+
+import time
+StartTime = time.time()
---
+++ WebStreamer/utils/__init__.py
@@ -0,0 +1,3 @@
+# This file is a part of TG-FileStreamBot
+# Coding : Jyothis Jayanth [@EverythingSuckz]
+# Maintained By : ThorXdshow [ @ThorXdshow ] [ Telegram ]
